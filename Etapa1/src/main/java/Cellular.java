a1316fcf3fa64283925a06b65e901af70585f0d3
M-Layseca <mlayseca@usm.cl>
2026-05-15 14:20:49 -0400

    Add files via upload

---
+++ Etapa1/src/main/java/Cellular.java
@@ -0,0 +1,6 @@
+public class Cellular extends Equipo {
+    public Cellular(String owner, double x, double y, double r, double theta, double dt) {
+        super(owner, x, y, r, theta, dt);
+    }
+
+}
